0cbe386af659791777c5f16010f3941a03597549
Nick Chan <nick.ccm1122@gmail.com>
2018-03-24 23:29:54 +0800

    fix: example story
    also add back .dockerignore

--- generators/app/templates/stories/ExampleComp/ExampleComp.story.js
+++ generators/app/templates/stories/ExampleComp/ExampleComp.story.js
@@ -2,7 +2,7 @@ import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 
-import { ExampleComp } from '../../components/ExampleComp'
+import ExampleComp from '../../components/ExampleComp'
 
 storiesOf('ExampleComp', module).add('default', () => (
   <ExampleComp onClickLink={type => action(`${type} link clicked`)} />
